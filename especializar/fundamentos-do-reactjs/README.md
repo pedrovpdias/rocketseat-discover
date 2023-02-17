10b0d15c2002f4eae5ca6235ad38199b26b98f5b
Pedro Vinícius P. Dias <pedro.dias@IFSMP.PAS.IFSULDEMINAS.EDU.BR>
2023-02-17 10:15:55 -0300

    Fundamentos do ReactJS - Concluído

--- especializar/fundamentos-do-reactjs/README.md
+++ especializar/fundamentos-do-reactjs/README.md
@@ -1,1 +1,23 @@
 # Fundamentos do ReactJS
+
+O **ReactJS** é uma biblioteca JavaScript para a criação de interfaces.
+
+* Não impõe uma estrutura de pastas e regras, assim como fazem os frameworks
+
+## O que vamos aprender?
+
+* [x] Criar projeto **ReactJS**
+    * [x] *Vite*
+* [x] Executar projeto
+* [x] Estrutura de pastas e arquivos
+* [x] Fragment `<></>`
+* [x] Importar arquivos *CSS*
+* [x] Componentes
+    * [x] *Props*
+* [x] Estado `useState()`
+    * [x] Imutabilidade
+* [x] *Key prop*
+* [x] Hooks
+* [x] `useEffect()`
+* [x] Consumir API
+* [x] Async `useEffect()`
--- especializar/fundamentos-do-reactjs/reactapp/src/pages/Home/index.jsx
+++ especializar/fundamentos-do-reactjs/reactapp/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 //Components
 import Card from '../../components/Card';
@@ -9,26 +9,101 @@ import './style.css';
 export default function Home() {
     //Estado: ['variavel', 'função_atribuir_valor'] = useState();
     const [studentName, setStudentName] = useState();
+    const [students, setStudents] = useState([]);
+
+    const [user, setUser] = useState({ name: '', avatar: ''});
+
+    function handleAddStudent() {
+        //Cria novo objeto
+        const newStudent = {
+            name: studentName,
+            time: new Date().toLocaleTimeString('pt-br', {
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+            })
+        }
+        
+        //Insere o novo objeto e recupera o conteúdo atual do estado
+        setStudents(previousState => [...previousState, newStudent])
+    }
+    
+    //useEffect(() => {}, []); // excutado automaticamente ao renderizar a interface
+    //[] = Array de dependencias: Quando vazio, executa o useEffect uma única vez
+    /*useEffect(() => {
+        const apiUrl = 'https://api.github.com/users/pedrovpdias';
+        
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                setUser({
+                    name: data.name,
+                    avatar: data.avatar_url
+                });
+            })
+            .catch(error => console.error(error));
+    }, []);*/
+
+    //Async useEffect
+    useEffect(() => {
+        const apiUrl = 'https://api.github.com/users/pedrovpdias';
+
+        async function fetchData() {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+
+            setUser({
+                name: data.name,
+                avatar: data.avatar_url
+            });
+        }
+
+        fetchData();
+    }, []);
     
     return (
         //Fragment: <></>
         <div className="container">
-            <h1>
-                Lista de Presença/ Nome: {studentName}
-            </h1>
+            <header>
+                <h1>
+                    Lista de Presença
+                </h1>
+
+                <div>
+                    <strong>
+                        {user.name}
+                    </strong>
+                    <img 
+                        src={user.avatar}
+                        alt="Foto de perfil"
+                    />
+                </div>
+            </header>
 
             <input 
                 type="text" 
                 placeholder="Nome:"
                 onChange={event => setStudentName(event.target.value)}
             />
-            <button type="button">
+            <button 
+                type="button" 
+                onClick={handleAddStudent}
+                autoFocus
+            >
                 Adicionar
             </button>
 
-            <Card name="Pedro" time="10:17:07" />
-            <Card name="José" time="10:25:21" />
-            <Card name="Maria Joana" time="16:20:00" />
+            {
+                students.map(student => {
+                    return (
+                        <Card
+                            key={Math.random()} //Key prop: Obrigatório
+                            name={student.name} 
+                            time={student.time} 
+                        />
+                    );
+                })               
+            }
         </div>
     )
 }
